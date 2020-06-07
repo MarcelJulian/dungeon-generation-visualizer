2e18d81b8e7df79d467a67d4dea6f8cb98e3eac7
MarcelJulian <marceljulian2047@gmail.com>
2020-06-07 22:26:58 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dungeon-generation-visualizer/precache-manifest.bcf579967cc03afd7cf5f4d0d7a46106.js"
+  "/dungeon-generation-visualizer/precache-manifest.90d3a33e38ad4474b9c989c7c1ad5f69.js"
 );
 
 self.addEventListener('message', (event) => {
